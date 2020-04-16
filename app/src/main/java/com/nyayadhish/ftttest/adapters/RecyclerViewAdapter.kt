dea70e169ec13694af147a65fa7444efaf025609
Nikhil <nyayadhish.nikhil20@gmail.com>
2020-04-16 09:36:10 +0530

    timer changes

--- app/src/main/java/com/nyayadhish/ftttest/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/nyayadhish/ftttest/adapters/RecyclerViewAdapter.kt
@@ -19,19 +19,6 @@ class RecyclerViewAdapter(private val mList: ArrayList<UserCardInfo>) :
     private lateinit var mContext: Context
     private val lstHolder: List<ViewHolder> = arrayListOf()
 
-    init {
-        //startUpdateTime()
-    }
-
-    private fun startUpdateTime() {
-        val t:Timer = Timer()
-        t.schedule(object: TimerTask(){
-            override fun run() {
-                handler.post(runnable)
-            }
-        },1000,1000)
-    }
-
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -46,67 +33,55 @@ class RecyclerViewAdapter(private val mList: ArrayList<UserCardInfo>) :
         )
     }
 
-    val handler = Handler()
-    private val runnable: Runnable = object : Runnable{
-        override fun run() {
-            synchronized(lstHolder){
-                for (holder in lstHolder){
-                    holder.updateTime()
-                }
-            }
-        }
-
-    }
-
     override fun getItemCount(): Int {
         return mList.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         if(mList.isNotEmpty()){
+            holder.setIsRecyclable(false)
             holder.itemView.tv_name.text = mList[position].name
             holder.itemView.tv_email.text = mList[position].email
             holder.itemView.start_time.text = String.format("%s %s","Start Time:",mList[position].start_time)
             holder.itemView.end_time.text = String.format("%s %s","End Time:",mList[position].end_time)
             holder.itemView.iv_user_image.setImageDrawable(mContext.getDrawable(mList[position].profileImage?:0))
             holder.itemView.counter.text = mList[position].time
-            //holder.setData(mList[position])
 
-            Handler().postDelayed({
+
+
+            holder.handler.postDelayed({
                 holder.timer = object: CountDownTimer(((mList[position].end_time?.toLong()?.minus(mList[position].start_time?.toLong()?:0))?:0)*1000,1000){
                     override fun onFinish() {
-                        mList.removeAt(position)
                         Log.i("List Size = ",mList.size.toString())
                         Log.i("List position = ",position.toString())
-                        notifyItemRemoved(position)
-                        //notifyDataSetChanged()
+                        if(position < mList.size) {
+                            mList.remove(mList[position])
+                            notifyItemRemoved(position)
+                            //notifyDataSetChanged()
+                            //notifyItemRangeChanged(position,itemCount.minus(position))
+                        }
                     }
 
                     override fun onTick(millisUntilFinished: Long) {
-                        holder.counter.text = (millisUntilFinished.div(1000)).toString()
+                        holder.updateText((millisUntilFinished.div(1000)).toString())
+
                     }
                 }
                 (holder.timer as CountDownTimer).start()
-            },mList[position].start_time?.toLong()?:0)
+            },(mList[position].start_time?.toLong()?:0)*1000)
         }
     }
 
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        fun updateText(time: String){
+            counter.text  = time
+        }
+
         var counter: TextView = view.findViewById(R.id.counter)
-
-        fun setData(cardInfo: UserCardInfo){
-            counter.text = cardInfo.time
-
-        }
-
-        fun updateTime() {
-
-        }
-
+        val handler = Handler()
         var timer: CountDownTimer? = null
-
-
     }
 }
 
--- app/src/main/java/com/nyayadhish/ftttest/viewmodels/FTTViewModel.kt
+++ app/src/main/java/com/nyayadhish/ftttest/viewmodels/FTTViewModel.kt
@@ -14,9 +14,9 @@ class FTTViewModel: ViewModel(){
         var mList: ArrayList<UserCardInfo> = arrayListOf()
         mList.add(UserCardInfo("Bruce Wayne","batman@jl.com", R.drawable.user_1,"0","30",""))
         mList.add(UserCardInfo("Barry Allen","barry@jl.com",R.drawable.user_1,"10","100",""))
-        mList.add(UserCardInfo("Clerk Kent","superman@jl.com",R.drawable.user_1,"20","30",""))
+        mList.add(UserCardInfo("Clerk Kent","superman@jl.com",R.drawable.user_1,"0","20",""))
         mList.add(UserCardInfo("Tony Stark","ironman@marvel.com",R.drawable.user_1,"40","100",""))
-        mList.add(UserCardInfo("Bruce Banner","hulk@marvel.com",R.drawable.user_1,"0","30",""))
+        mList.add(UserCardInfo("Bruce Banner","hulk@marvel.com",R.drawable.user_1,"25","30",""))
         mList.add(UserCardInfo("Peter Parker","spiderman@marvel.com",R.drawable.user_1,"20","100",""))
 
         testDataLive.value = mList
